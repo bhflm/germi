473935e973614b7e95bf2d06442b33ceb652c2a1
Germi <germanbonin@gmail.com>
2020-03-29 20:59:38 -0300

    add: that wasnt that good at all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,11 @@ import { TwitterTimeline } from '../components/Twitter';
 
 const Home: NextPage<{ props: any }> = props => (
   // <TwitterTimeline/> not gonna use this atm
+  // header
+  // main layout
+  // footer
   <div>
-    welcome!
+    QUEDENSE EN SUS CASAS Y LAVENSE LAS MANOS
   </div>
 );
 
