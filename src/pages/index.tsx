610e3ac861631c6cc90641635b5c5df04d117277
German Bonin <germi@Germans-MacBook-Pro.local>
2020-03-29 20:50:28 -0300

    remove: twitter tl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import { NextPage } from 'next';
 import { TwitterTimeline } from '../components/Twitter';
 
 const Home: NextPage<{ props: any }> = props => (
-  <TwitterTimeline/>
+  // <TwitterTimeline/> not gonna use this atm
+  <div>
+    welcome!
+  </div>
 );
 
 export default Home;
