55382f399c8659f2fad55613042f2e4b679caac4
German Bonin <germanbonin@gmail.com>
2020-04-28 19:04:27 -0300

    fix: css styling for navbar graphic design is my passion

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,20 @@
 import React, { FunctionComponent } from "react";
 
 export const Navbar: FunctionComponent<{}> = () => (
-    <div className="">
-        <p className="text-sans">som navbar text</p>
-    </div>  
+    <div className="flex justify-between flex-wrap">
+        <div className="block">
+          <p className="">icon</p>
+        </div>
+        <nav className="flex space-between mx-r">
+          <a className="text-xl" href="#">
+            som link
+          </a>
+          <a className="text-xl" href="#">
+            otter link
+          </a>
+          <a className="text-xl top-0 right-0" href="#">
+            moar link
+          </a>
+        </nav>
+    </div>
 );
