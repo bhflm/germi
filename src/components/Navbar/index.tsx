fa2802d434a392be6220ab897bb7e1403f48393a
German Bonin <germanbonin@gmail.com>
2020-04-29 19:17:42 -0300

    add: github icon, arcaic navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,19 @@
 import React, { FunctionComponent } from "react";
 
 export const Navbar: FunctionComponent<{}> = () => (
-    <div className="flex justify-between flex-wrap">
-        <div className="block">
-          <p className="">icon</p>
+    <div className="flex px-4 justify-between flex-wrap border-b-2 border-black">
+        <div className="text-xl px-4">
+          grmi
         </div>
         <nav className="flex space-between mx-r">
-          <a className="text-xl px-4" href="#">
-            som link
+          <a className="text-xl px-4" href="/archive">
+            archive
           </a>
-          <a className="text-xl px-4" href="#">
-            otter link
+          <a className="text-xl px-4" href="/about">
+            about
           </a>
-          <a className="text-xl px-4" href="#">
-            moar link
+          <a href="https://github.com/bhflm" target="_blank">
+            <img className="h-8 pt-1 pb-1 px-4" src="/github.svg" alt="github logo"/>
           </a>
         </nav>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { Navbar } from '../components/Navbar';
 const Home: NextPage<{ props: any }> = props => (
   <>
   <Navbar/>
-  <span>my site</span>
   </>
 );
 
