8ab5fe8742786862ea3a2340f3873021b5e22fee
German Bonin <germanbonin@gmail.com>
2020-04-29 18:36:34 -0300

    trigger deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import { NextPage } from 'next';
 import { Navbar } from '../components/Navbar';
 
 const Home: NextPage<{ props: any }> = props => (
+  <>
   <Navbar/>
+  <span>my site</span>
+  </>
 );
 
 export default Home;
