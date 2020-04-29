04fc8fa80d3861ce0a0e03ab3e72c4db5891fe28
German Bonin <germanbonin@gmail.com>
2020-04-29 18:29:48 -0300

    modif postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./src/pages/**/*.{js,jsx,ts,tsx}",
+      "./src/components/**/*.{js,jsx,ts,tsx}"
+    ],
+    defaultExtractor: content => content.match(/[w-/:]+(?<!:)/g) || []
+  }
+];
+
 module.exports = {
   plugins: [
+    "postcss-import",
     "tailwindcss",
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: [
-              "./pages/**/*.{js,jsx,ts,tsx}",
-              "./components/**/*.{js,jsx,ts,tsx}"
-            ],
-            defaultExtractor: content => content.match(/[w-/:]+(?<!:)/g) || []
-          }
-        ]
-      : undefined,
-    "autoprefixer"
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
