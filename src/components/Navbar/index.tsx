091bdda667d1f01cfd66f8c82c70c815a53945c3
German Bonin <germanbonin@gmail.com>
2020-04-29 19:35:07 -0300

    add: mountain icon and mor css styl;ing

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,13 +3,13 @@ import React, { FunctionComponent } from "react";
 export const Navbar: FunctionComponent<{}> = () => (
     <div className="flex px-4 justify-between flex-wrap border-b-2 border-black">
         <div className="text-xl px-4">
-          grmi
+          <img className="h-12 pt-2 pb-2" src="/mountain.svg" alt="mountain"/>
         </div>
-        <nav className="flex space-between mx-r">
-          <a className="text-xl px-4" href="/archive">
+        <nav className="flex space-between mx-r pt-2">
+          <a className="text-xl px-4 pt-1" href="/archive">
             archive
           </a>
-          <a className="text-xl px-4" href="/about">
+          <a className="text-xl px-4 pt-1" href="/about">
             about
           </a>
           <a href="https://github.com/bhflm" target="_blank">
