db628c501c42c885a9699617ef1810ae480d6588
German Bonin <germanbonin@gmail.com>
2020-05-02 22:13:28 -0300

    add: firebase set up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from 'next';
+import Head from 'next/head'
 import { Navbar } from '../components/Navbar';
 import { Layout } from '../components/Layout';
 
 const Home: NextPage<{ props: any }> = props => (
   <>
+  <Head>
+    <title>grmi notes</title>
+  </Head>
   <Navbar/>
   <Layout />
   </>
