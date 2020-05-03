b5e01c7ae2496c10bbb0a42a69d246a0eeb09ddb
German Bonin <germanbonin@gmail.com>
2020-05-02 23:23:06 -0300

    add page entry prototype

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,7 +1,35 @@
-import React, { FunctionComponent } from "react";
+import { NextPage } from 'next';
+import Head from 'next/head'
+import { Layout } from '../../components/Layout';
+import db from '../../clients/firebase';
 
-export const Layout: FunctionComponent<{}> = () => (
-    <div className="pt-20">
-        <p className="text-center h-8 black">...under construction...</p>
-    </div>
+interface Props {
+  title?: string;
+  tags?: string[];
+  content?: string;
+};
+
+
+const PostTemplate: NextPage<Props> = ({ title, tags, content }) => (
+  <div>
+  <article>
+    <h1>{title}</h1>
+    <span>
+      {content}
+    </span>
+  </article>
+  </div>
 );
+
+PostTemplate.getInitialProps = async ({ query }) => {
+  let resData = {};
+  const dbRef = db.collection('posts');
+  await dbRef.doc('first-post').get().then( snapshot => {
+    resData = snapshot.data();
+  })
+  console.log('RES DATA; ', resData);
+  return resData;
+};
+
+
+export default PostTemplate;
