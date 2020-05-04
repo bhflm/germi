dd6e25bc83dfefbfd638b009ba4c536e2b0a7ddf
German Bonin <germanbonin@gmail.com>
2020-05-03 22:31:01 -0300

    making ts dirty as me

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,45 @@ import { NextPage } from 'next';
 import Head from 'next/head'
 import { Navbar } from '../components/Navbar';
 import { Layout } from '../components/Layout';
+import loadFirebaseClient from '../clients/firebase';
 
-interface Props {
-  query?: string;
+interface Post {
+  title?: string,
+  ref?: string,
 };
 
+interface HomeProps {
+  posts?: Array<Post>[];
+}
 
-const Home: NextPage<Props> = query => (
-  <>
-  <Head>
-    <title>grmi notes</title>
-  </Head>
-  <Navbar/>
-  <Layout />
-  </>
-);
+const Home: NextPage<HomeProps> = (data) => {
+  const posts = data as Array<any> // dont tell anyone i did this
+  return (
+    <>
+    <Head>
+      <title>grmi notes</title>
+    </Head>
+      {posts.map(each =>(<p>{each.title}</p>))}
+    <Navbar/>
+    <Layout />
+    </>
+  );
+};
 
-Home.getInitialProps = async ({ query }) => {
-  let resData = {};
-  // TODO 
-  // STRUCTURE POSTS DATA TO FETCH A LIST SO WE AVOID NESTED ACCESS FOR POSTS LIST
-  return resData;
+Home.getInitialProps = async () => {
+    let responseData = [];
+    const dbRef = await loadFirebaseClient();
+    const querySnapshot = await dbRef.collection('home');
+    await querySnapshot.get()
+          .then((snapshot) => {
+            snapshot.forEach(snap => {
+              const data = snap.data();
+              console.log('DATA; ', data);
+              const postInfo = { name: data.name, ref: data.ref };
+              responseData.push(postInfo); 
+            })
+          }).catch(Promise.reject);
+    return responseData as unknown; // i feel dirty and i need to solve this because i can't type good enough
 };
 
 
