9a8755f0245652ee73064d2f34bef0e0f5a74997
German Bonin <germanbonin@gmail.com>
2020-10-27 01:35:14 -0300

    little update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,8 +6,11 @@ export const Navbar = () => (
           <img className="h-12 pt-2 pb-2" src="/mountain.svg" alt="mountain"/>
         </div>
         <nav className="flex space-between mx-r pt-2">
+          <a href="https://www.notion.so/German-Bonin-32f5076d93fc4acfa996e8a92f5dca08" target="_blank">
+              <span className="font-semibold px-4">about</span>
+          </a>
           <a href="https://github.com/bhflm" target="_blank">
-            <img className="h-8 pt-1 pb-1 px-4" src="/github.svg" alt="github logo"/>
+            <img className="h-8 pt-1 pb-2" src="/github.svg" alt="github logo"/>
           </a>
         </nav>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,28 @@ const Home = () => {
       <title>grmi notes</title>
     </Head>
     <Navbar/>
-    <Layout />
+    <div>
+      <h1 className="font-semibold ml-20 pt-20 text-left h-8 black">
+        Hey there,
+      </h1>
+      <div className="font-small mt-16 ml-20 text-left h-9 black">
+        <span>
+          I'm a software developer from Buenos Aires, Argentina.
+          <br></br>
+          <br></br>
+          Currently working as a remote contractor for <a className="font-semibold" target="_blank" href="https://grow.com">Grow</a>, an awesome Utah BI based company.
+          <br></br>
+          <br></br>
+          Oh, yes, I do not have good taste for design at all, I'm working on improving that (really).
+          <br></br>
+          <br></br>
+          But I'm interested in backend, security, and blockchain stuff, (I find the latter pretty awesome tbh).
+          <br></br>
+          <br></br>
+          Feel free to reach me <a className="font-semibold" href="mailto:germanbonin@gmail.com">here</a> anytime.
+        </span>
+      </div>
+    </div>
     </>
   );
 };
